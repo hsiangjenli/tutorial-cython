02624e06e42c4332e940000b672e40459d10c47a
RN <hsiangjenli@gmail.com>
2023-04-08 14:12:43 +0800

    feat(02_hello_world): how to print hello world in cython

---
+++ prac/02_hello_world/README.md
@@ -0,0 +1,6 @@
+# 🚀 **Usage**
+```shell
+cd prac\02_hello_world
+python setup.py build_ext --inplace # cython to C
+python demo.py # run demo file
+```
---
+++ prac/02_hello_world/hello_world.pyx
@@ -0,0 +1,15 @@
+"""
+# 什麼是 Cython?
+
+!!! note cython 的特點
+    1. Python 語法相容
+    2. C 語言的速度
+    3. 靜態類型聲明
+    4. 同時支援 C 與 Python 第三方 pkg
+
+"""
+import cython
+
+def print_hello_world():
+
+    print("Hello World.")
