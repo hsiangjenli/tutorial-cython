2351094f1231569ed68d8d0218999399d4a9ddc6
RN <hsiangjenli@gmail.com>
2023-04-08 19:50:29 +0800

    chore(03_data_types): remove unused code

--- prac/03_data_types/basic.pyx
+++ prac/03_data_types/basic.pyx
@@ -34,18 +34,3 @@ def print_float(double input_float):
         ,
         this_is_double, this_is_float
     )
-
-# def print_different_type_of_var(unsigned int this_is_integer):
-#     cdef char* this_is_string = "This is a string"
-#     cdef float this_is_float = 3.14
-#     cdef double this_is_double = 3.141592653589793238462643383279502884197169399375105820974944592307816406286
-#     print(
-#         f"""
-
-#         {type(this_is_integer)} = {this_is_integer} 
-#         {type(this_is_string)} = {this_is_string}
-#         {type(this_is_float)} = {this_is_float}
-
-#         """
-#     )
-#     return True
